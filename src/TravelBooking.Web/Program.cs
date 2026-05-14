057d11884f2d147994a29f83f246f7b4cab07f8c
sridattaga <sivag.soc95@gmail.com>
2026-05-14 12:10:49 +0530

    Update Program.cs

--- src/TravelBooking.Web/Program.cs
+++ src/TravelBooking.Web/Program.cs
@@ -1,20 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using TravelBooking.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=travel.db"));
+    options.UseInMemoryDatabase("TravelBookingDb"));
 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Configure middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
