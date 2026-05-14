6a0af6ef4c4701bbc7513621eefaac92df8c21ee
sridattaga <sivag.soc95@gmail.com>
2026-05-14 10:13:02 +0530

    Create Hotel.cs

---
+++ src/TravelBooking.Web/Models/Hotel.cs
@@ -0,0 +1,9 @@
+namespace TravelBooking.Web.Models;
+
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public decimal PricePerNight { get; set; }
+}
