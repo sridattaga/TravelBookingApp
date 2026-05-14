57dcbab2bd903502ce6d7e85481d88bbacd9668c
sridattaga <sivag.soc95@gmail.com>
2026-05-14 12:11:33 +0530

    Refactor AppDbContext to use properties for DbSets

--- src/TravelBooking.Web/Data/AppDbContext.cs
+++ src/TravelBooking.Web/Data/AppDbContext.cs
@@ -1,14 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using TravelBooking.Web.Models;
 
-namespace TravelBooking.Web.Data;
-
-public class AppDbContext : DbContext
+namespace TravelBooking.Web.Data
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options) {}
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
 
-    public DbSet<Hotel> Hotels => Set<Hotel>();
-    public DbSet<Car> Cars => Set<Car>();
-    public DbSet<Booking> Bookings => Set<Booking>();
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
 }
